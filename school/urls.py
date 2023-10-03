518d0871dc930ca16d8c8624cfd0f5bc8e5788ff
IlyaKovalchukKIS <mrlivedance@vk.com>
2023-10-03 23:51:20 +0300

    Добавлена маршрутизация для добавления способа оплаты

--- school/urls.py
+++ school/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from school.apps import SchoolConfig
 from school.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
-    LessonUpdateAPIView, LessonDestroyAPIView
+    LessonUpdateAPIView, LessonDestroyAPIView, PaymentCreateAPIView
 
 app_name = SchoolConfig.name
 
@@ -15,4 +15,6 @@ urlpatterns = [
     path('lesson/detail/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_detail'),
     path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson_update'),
     path('lesson/destroy/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson_destroy'),
+
+    path('lesson/payment/create/', PaymentCreateAPIView.as_view(), name='payment_create'),
 ] + router.urls

69b07419887190d21dd8bd52d13daa6e884d5308
IlyaKovalchukKIS <mrlivedance@vk.com>
2023-10-08 16:25:36 +0300

    Добавлено поле вывода количества уроков

--- school/serializers.py
+++ school/serializers.py
@@ -11,10 +11,15 @@ class LessonSerializer(serializers.ModelSerializer):
 
 class CourseSerializer(serializers.ModelSerializer):
     lesson_set = LessonSerializer(many=True, read_only=True)
+    total_lessons = serializers.SerializerMethodField()
+
+    def get_total_lessons(self, obj):
+        return obj.lesson_set.count()
 
     class Meta:
         model = Course
         fields = '__all__'
+
 
 
 class PaymentSerializer(serializers.ModelSerializer):
